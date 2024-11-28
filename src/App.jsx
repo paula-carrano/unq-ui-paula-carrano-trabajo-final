786d92bf6aee87481bc0b10799f144577ba91223
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2024-11-27 21:09:34 -0300

    init proyect

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
