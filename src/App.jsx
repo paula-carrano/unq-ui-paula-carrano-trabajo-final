36c4fa05901f1392a31b78280d6de119e4ec4b68
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2024-11-28 17:34:49 -0300

    img

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./Screens/Home";
-import { BoardOptions } from "./Components/BoardOptions";
-import { Score } from "./Components/Score";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/:mode" element={<BoardOptions />} />
-      <Route path="/score" element={<Score />} />
     </Routes>
   );
 }
