c504fa46b91fb851cc83d1a60b99f26427c864ce
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2024-12-06 15:15:59 -0300

    modal v2

--- src/components/ModalScore/ModalScore.jsx
+++ src/components/ModalScore/ModalScore.jsx
@@ -1,23 +1,34 @@
-// ModalScore Component
 import React from "react";
 import Modal from "react-modal";
-
 import "./modalScore.css";
 
+// Establecer el elemento de la aplicación para mejorar la accesibilidad
 Modal.setAppElement("#root");
 
 export const ModalScore = ({ isOpen, onClose, winner, winnerScore, mode }) => {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose}>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className="modal-content"
+      overlayClassName="modal-overlay"
+      aria-labelledby="modalTitle"
+      aria-describedby="modalDescription"
+    >
       <div className="modal-content">
-        <h2>{mode === "singleplayer" ? "Game Over!" : "Winner!"}</h2>
-        {mode === "singleplayer" ? (
-          <p>Your score: {winnerScore}</p>
-        ) : (
-          <p>
-            {winner} wins with {winnerScore} points!
-          </p>
-        )}
+        <h2 id="modalTitle">
+          {mode === "singleplayer" ? "Game Over!" : "Winner!"}
+        </h2>
+        <p id="modalDescription">
+          {mode === "singleplayer" ? (
+            <span>Your score: {winnerScore}</span>
+          ) : (
+            <span>
+              <strong>{winner}</strong> wins with <strong>{winnerScore}</strong>{" "}
+              points!
+            </span>
+          )}
+        </p>
         <button onClick={onClose}>Back to Start</button>
       </div>
     </Modal>
