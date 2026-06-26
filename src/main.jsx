e163faadcb0617e484239981a945ba3aab4649db
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2026-06-26 13:57:04 -0300

    add css a main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
