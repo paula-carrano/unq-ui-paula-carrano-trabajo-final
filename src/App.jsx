c5a63326b61065b18d3c4de7473f420037db51ad
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2026-06-26 13:36:02 -0300

    add layout basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Home, ErrorPage, GameOver, Game, LeaderBoard } from "./pages/index.js";
 import { Routes, Route } from "react-router-dom";
+import { PageLayout } from "./components";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/game" element={<Game />} />
-            <Route path="/game-over" element={<GameOver />} />
-            <Route path="/leaderboard" element={<LeaderBoard />} />
-            <Route path="*" element={<ErrorPage />} />
-        </Routes>
+        <PageLayout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="/game-over" element={<GameOver />} />
+                <Route path="/leaderboard" element={<LeaderBoard />} />
+                <Route path="*" element={<ErrorPage />} />
+            </Routes>
+        </PageLayout>
     );
 }
 
---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,7 @@
+export const PageLayout = ({ children }) => {
+    return (
+        <main className="min-vh-100 border rounded-3 bg-light px-3 py-4">
+            {children}
+        </main>
+    );
+};
