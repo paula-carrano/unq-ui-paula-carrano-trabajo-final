8907ec3027be8ee337915a459f726bcd9b6505db
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2026-06-26 14:54:54 -0300

    pageError sin styles

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,3 +1,23 @@
+import { RefreshCcw, House, TriangleAlert } from "lucide-react";
+import { Container, Button } from "react-bootstrap";
+
 export const ErrorPage = () => {
-    return <div> soy un error page</div>;
+    return (
+        <Container fluid>
+            <TriangleAlert className="text-danger" size={100} />
+            <h2>¡Ups! algo salió mal</h2>
+            <h4>No pudimos conectar con el servidor</h4>
+            <h4>Por favor, verifica tu conexión e intenta nuevamente.</h4>
+            <Button variant="primary" onClick={() => window.location.reload()}>
+                <RefreshCcw />
+                Reintentar
+            </Button>
+            <Button
+                variant="outline-secondary"
+                onClick={() => (window.location.href = "/")}
+            >
+                <House /> Ir al inicio
+            </Button>
+        </Container>
+    );
 };
