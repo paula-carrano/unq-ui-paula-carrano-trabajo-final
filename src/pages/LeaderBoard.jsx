7de7743eafaf46b94b5d9bc9bc1ef45b2bf995f6
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2026-06-27 08:45:30 -0300

    v1 mejores puntajes sin logica y styles

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,3 +1,31 @@
+import { Medal, MoveLeft } from "lucide-react";
+import { Container, ListGroup } from "react-bootstrap";
+
 export const LeaderBoard = () => {
-    return <div> soy un leaderboard page</div>;
+    const puntajes = [
+        { nombre: "Ana", puntaje: 50 },
+        { nombre: "Luis", puntaje: 40 },
+        { nombre: "Marta", puntaje: 30 },
+        { nombre: "Juan", puntaje: 20 },
+        { nombre: "Sofia", puntaje: 10 },
+    ];
+    const puntajesCompletos = [
+        ...puntajes,
+        ...Array(10).fill({ nombre: "-", puntaje: 0 }),
+    ].slice(0, 10);
+
+    return (
+        <Container>
+            <MoveLeft />
+            <h1> Mejores Puntajes</h1>
+            <ListGroup as="ol" numbered>
+                {puntajesCompletos.map((puntaje, index) => (
+                    <ListGroup.Item key={index}>
+                        {index < 3 && <Medal />}
+                        {puntaje.nombre} {puntaje.puntaje}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+        </Container>
+    );
 };
