e4d609db89aa561e4806442c0816a747dceaed4e
Paula Carrano <65313769+paula-carrano@users.noreply.github.com>
2026-06-27 11:41:55 -0300

    add style a leaderboard

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,4 +1,5 @@
 import { Medal, MoveLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 import { Container, ListGroup } from "react-bootstrap";
 
 export const LeaderBoard = () => {
@@ -15,17 +16,44 @@ export const LeaderBoard = () => {
     ].slice(0, 10);
 
     return (
-        <Container>
-            <MoveLeft />
-            <h1> Mejores Puntajes</h1>
-            <ListGroup as="ol" numbered>
-                {puntajesCompletos.map((puntaje, index) => (
-                    <ListGroup.Item key={index}>
-                        {index < 3 && <Medal />}
-                        {puntaje.nombre} {puntaje.puntaje}
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
+        <Container
+            fluid
+            className="d-flex flex-column h-100"
+            style={{ minHeight: "calc(100vh - 6rem)" }}
+        >
+            <div className="position-relative d-flex align-items-center justify-content-center">
+                <Link className="text-dark position-absolute start-0" to="/">
+                    <MoveLeft />
+                </Link>
+                <h1 className="mb-3 text-uppercase fw-bold lh-sm fs-3 text-dark">
+                    {" "}
+                    Mejores Puntajes
+                </h1>
+            </div>
+            <div
+                className="mx-auto mt-4 flex-grow-1 d-flex"
+                style={{ maxWidth: "60%", width: "100%" }}
+            >
+                <ListGroup
+                    as="ol"
+                    numbered
+                    variant="flush"
+                    className="w-100 d-flex flex-column"
+                >
+                    {puntajesCompletos.map((puntaje, index) => (
+                        <ListGroup.Item
+                            key={index}
+                            className="flex-fill d-flex align-items-center justify-content-between"
+                        >
+                            <span>
+                                {index < 3 && <Medal />}
+                                {puntaje.nombre}
+                            </span>
+                            <span>{puntaje.puntaje}</span>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </div>
         </Container>
     );
 };
